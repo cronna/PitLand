d2fa9c60f9d1d33a596b060ecf129762e865f6cb
Said Akhmedov <crona8875@gmail.com>
2024-06-19 17:24:10 +0400

    commit

--- entity/Categories.php
+++ entity/Categories.php
@@ -15,8 +15,51 @@ use Yii;
  */
 class Categories extends \yii\db\ActiveRecord
 {
-    public function getMenus()
+    <?php
+
+namespace app\entity;
+
+use Yii;
+
+/**
+ * This is the model class for table "categories".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string|null $description
+ *
+ * @property Menu[] $menus
+ */
+class Categories extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
     {
-        return $this->hasMany(Menu::class, ['category_id' => 'id']);
+        return 'categories';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title'], 'required'],
+            [['title'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'description' => 'Description',
+        ];
     }
 }
